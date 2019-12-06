3a00d12b330bc51d394498a61d709687a716a95a
jss2019 <543729>
2019-12-06 14:54:13 +0800

    test

---
+++ src/main/java/com/js/MySpringbootApp.java
@@ -0,0 +1,14 @@
+package com.js;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  //声明这个类是Springboot引导类
+public class MySpringbootApp {
+
+    public static void main(String[] args) {
+        //run方法 表示运行SpringBoot的引导类 run参数就是SpringBoot引导类的字节码对象
+        SpringApplication.run(MySpringbootApp.class);
+    }
+
+}
---
+++ src/main/java/com/js/controler/QucikController.java
@@ -0,0 +1,16 @@
+package com.js.controler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class QucikController {
+
+    @RequestMapping("/quick")
+    @ResponseBody
+    public String quick(){
+        return "hello springboot";
+    }
+
+}
